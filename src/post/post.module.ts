df3ec66e368baed62c448ddd64e6a90a7d212781
tanhun <tanhoang0710@gmail.com>
2023-04-05 23:00:29 +0700

    #03: MongoDB with NestJS - Model

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PostController } from './controller/post.controller';
 import { PostService } from './services/post.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostSchema } from './model/post.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Post',
+        schema: PostSchema,
+      },
+    ]),
+  ],
   controllers: [PostController],
   providers: [PostService],
 })
